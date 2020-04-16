e326392665269b08d87acab3cbcc429ee6421cb7
Loremar Marabillas <loremar5858@gmail.com>
2020-04-16 18:48:11 +0800

    Updates

---
+++ precache-manifest.b64d95887d92f8a495c4f5e0aa6c2667.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e4468d68d6990d5ca129a2134fcd05",
+    "url": "/FCC-Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "3e84c79893dfd18dac95",
+    "url": "/FCC-Markdown-Previewer/static/css/2.b8ee247f.chunk.css"
+  },
+  {
+    "revision": "d1b40368e70f806d554b",
+    "url": "/FCC-Markdown-Previewer/static/css/main.fc8c85a4.chunk.css"
+  },
+  {
+    "revision": "3e84c79893dfd18dac95",
+    "url": "/FCC-Markdown-Previewer/static/js/2.f9c245d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FCC-Markdown-Previewer/static/js/2.f9c245d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b40368e70f806d554b",
+    "url": "/FCC-Markdown-Previewer/static/js/main.3a04cd8d.chunk.js"
+  },
+  {
+    "revision": "5585096e138c4bad2a29",
+    "url": "/FCC-Markdown-Previewer/static/js/runtime-main.941a5b8f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FCC-Markdown-Previewer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FCC-Markdown-Previewer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FCC-Markdown-Previewer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FCC-Markdown-Previewer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FCC-Markdown-Previewer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
